2aa55bf2bc4f3baf9f736c78e64b43f678584921
Liubomyr Demianchuk <demianchukliubomyr@gmail.com>
2022-06-08 13:40:04 +0200

    implemented methods, which get data from service and show fighter info

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -4,25 +4,38 @@ export function createFighterPreview(fighter, position) {
   const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
   const fighterElement = createElement({
     tagName: 'div',
-    className: `fighter-preview___root ${positionClassName}`,
+    className: `fighter-preview___root ${positionClassName}`
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    const { name, health, attack, defense } = fighter;
+    const fighterImage = createFighterImage(fighter);
+    const fighterName = createElement({ tagName: 'div', className: 'fighter-preview___description' });
+    fighterName.textContent = `Name: ${name}`;
+    const fighterAttack = createElement({ tagName: 'div', className: 'fighter-preview___description' });
+    fighterAttack.textContent = `Attack: ${attack}`;
+    const fighterDefense = createElement({ tagName: 'div', className: 'fighter-preview___description' });
+    fighterDefense.textContent = `Defense: ${defense}`;
+    const fighterHealth = createElement({ tagName: 'div', className: 'fighter-preview___description' });
+    fighterHealth.textContent = `Health: ${health}`;
+
+    fighterElement.append(fighterImage, fighterName, fighterHealth, fighterAttack, fighterDefense);
+  }
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
+  const attributes = {
+    src: source,
     title: name,
-    alt: name 
+    alt: name
   };
   const imgElement = createElement({
     tagName: 'img',
     className: 'fighter-preview___img',
-    attributes,
+    attributes
   });
 
   return imgElement;
